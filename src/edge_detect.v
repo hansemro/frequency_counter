80d307223bbacf34bf44f4d44b48bb6b8b1e32c7
Hansem Ro <hansemro@outlook.com>
2022-05-08 17:25:08 -0700

    Add src/edge_detect.v solution

--- src/edge_detect.v
+++ src/edge_detect.v
@@ -1,11 +1,22 @@
 `default_nettype none
 `timescale 1ns/1ps
+
+// Note: this design already buffers input with two flip flops
+
 module edge_detect (
     input wire              clk,
     input wire              signal,
     output wire             leading_edge_detect
     );
 
-    assign leading_edge_detect = 0;
+    reg q0, q1, q2;
+
+    assign leading_edge_detect = q1 & (q1 ^ q2);
+
+    always @(posedge clk) begin
+        q0 <= signal;
+        q1 <= q0;
+        q2 <= q1;
+    end
 
 endmodule
